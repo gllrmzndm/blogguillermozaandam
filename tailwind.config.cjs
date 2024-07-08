6902d952167f8c16d6cb4e7f0e0bd3773e2f109e
gllrmzndm <guillermozaandam@hotmail.com>
2024-07-08 15:01:33 +0200

    new theme with purple hovers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        herobackgroundimage: "url('/public/assets/tim.jpg')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
