49fe2df99912fc45f1b24d2dd60d9bc571846450
gllrmzndm <guillermozaandam@hotmail.com>
2024-12-05 21:01:27 +0100

    Disabled google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      
       backgroundImage: {
         herobackgroundimage: "url('/assets/nav_hero.jpg')",
       },
